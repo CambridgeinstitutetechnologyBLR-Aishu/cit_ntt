878e3ccb5825aa496c036ac71d684fd7823f0ffd
CambridgeinstitutetechnologyBLR-Aishu <aishwarya.24ece@cambridge.edu.in>
2026-05-27 22:46:29 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -11,10 +11,13 @@ module tt_um_pqc_aishu (
     input  wire       rst_n     
 );
 
-    // Pure combinational logic - No clock used
-    // This adds the two 4-bit halves of the input
-    assign uo_out = ui_in[7:4] + ui_in[3:0];
-    
+    // Combinational math
+    wire [7:0] math_result = ui_in[7:4] + ui_in[3:0];
+
+    // THE TRICK: Use clk in a way that doesn't change the math
+    // but forces the tool to 'see' the pin.
+    assign uo_out = math_result ^ {7'b0, (clk & 1'b0)}; 
+
     assign uio_out = 8'b0;
     assign uio_oe  = 8'b0;
 
